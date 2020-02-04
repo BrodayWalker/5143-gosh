15d2c7ef8a434187bace2b2d289aeb4fe2a39d29
Jeremy Glebe <jeremyglebe@gmail.com>
2020-02-03 18:48:37 -0600

    implemented command getting

---
+++ gosh.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	// Main loop
+	for {
+		command, args := getCommand()
+		fmt.Println(command)
+		for _, element := range args {
+			fmt.Println(element)
+		}
+	}
+}
+
+func getCommand() (string, []string) {
+	// Create a keyboard reader
+	keyboard := bufio.NewReader(os.Stdin)
+	// Read a line of input
+	line, e := keyboard.ReadString('\n')
+	// Print out any errors
+	if e != nil {
+		fmt.Fprintln(os.Stderr, e)
+	}
+	line = strings.Trim(line, "\n")
+	// Separate the arguments
+	input := strings.Split(line, " ")
+	command := input[0]
+	args := input[1:]
+	// Return command and arguments
+	return command, args
+}
