38fb406aafb36c0cfad3c505b58ff0ad76ec2237
Pirhomega <35641464+Pirhomega@users.noreply.github.com>
2020-02-05 17:32:28 -0600

    tested permission bit code

--- chmod/chmod.go
+++ chmod/chmod.go
@@ -4,28 +4,42 @@ package main
 //after 'chmod '. It will process the options by splitting the arguments string
 //and then iteratively traversing each substring.
 
+//TO COMPILE AND EXECUTE THE CODE: go build; ./chmod
+//Make sure to navigate to the chmod directory before building and executing
+
+//**************************************************************************//
+//NEED TO FIND A WAY TO MODIFY PERMISSION BITS
+//**************************************************************************//
+
 import (
 	"fmt"
+	"log"
 	"os"
-	"strings"
+	//"strings"
 )
 
 //chmod u=rwx,g=rx,o=r myfile
 
 func main() {
-	initial := "577 oogabooga.exe"
-	input := strings.Fields(initial) //split string apart by whitespaces
+	//initial := "577 oogabooga.exe"
+	//input := strings.Fields(initial) //split string apart by whitespaces
 	// fmt.Println(string(input[0]))
 	// fmt.Println(string(input[2]))
 	// fmt.Println(string(input[7]))
 	// fmt.Println(strings.Fields(input)[1])
 
-	for _, argument := range input {
-		fmt.Println("This is the argument: ", string(argument))
-		for _, character := range string(argument) {
-			fmt.Println(string(character))
-		}
+	test1, err := os.Lstat("C:\\Users\\Owner\\Desktop\\5143-OS-Matamoros\\5143-gosh\\chmod")
+	if err != nil {
+		log.Fatal(err)
 	}
+	fmt.Printf("permissions: %#o\n", test1.Mode().Perm()) //https://golang.org/pkg/os/?m=all#fileStat
+
+	// for _, argument := range input {
+	// 	fmt.Println("This is the argument: ", string(argument))
+	// 	for _, character := range string(argument) {
+	// 		fmt.Println(string(character))
+	// 	}
+	// }
 
 	//easy part 1: if someone just typed 'chmod' with no arguments
 
