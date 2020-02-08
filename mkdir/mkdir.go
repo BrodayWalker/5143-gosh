5a76be4946392ff04735e97fe6710ae4bf8c8cdc
Broday Walker <brodaywalker@gmail.com>
2020-02-07 20:01:02 -0600

    mkdir test passed

--- mkdir/mkdir.go
+++ mkdir/mkdir.go
@@ -2,19 +2,33 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
+	"syscall"
 )
 
 func main() {
-	path := ".\\test"
-	var mode os.FileMode = 0755
-
-	err := os.Mkdir(path, mode)
-
-	fmt.Printf("%T", err)
+	// Get the current working directory
+	path, err := os.Getwd()
 
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println("Failed to get current working directory.")
+	}
+	folderName := "test"
+
+	// Construct absolute path
+	totalPath := path + "\\" + folderName
+
+	// Print path for testing
+	fmt.Println(totalPath)
+
+	// Convert string totalPath to a *uint16
+	totalPathPtr := syscall.StringToUTF16Ptr(totalPath)
+	// Make the directory using the Windows CreateDirectory system call
+	errPath := syscall.CreateDirectory(totalPathPtr, nil)
+
+	if errPath == syscall.ERROR_ALREADY_EXISTS {
+		fmt.Println("Directory already exists.")
+	} else if errPath == syscall.ERROR_PATH_NOT_FOUND {
+		fmt.Println("Failed to create directory. One or more intermediate directories do not exist.")
 	}
 }
