cfde5b7bc36b8c7e2874f1c049d78723c6b2f76c
Broday Walker <brodaywalker@gmail.com>
2020-02-10 08:32:55 -0600

    added shell prompt and basic mkdir functionality

---
+++ gosh/gosh.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"runtime"
+	"strings"
+)
+
+func main() {
+	// Main loop
+	for {
+		// Print prompt
+		// The double percent sign must be used to print a literal percent sign
+		fmt.Printf("%% ")
+		// Get the command
+		command, args := parseCommand(getInput())
+		// Standardize command
+		command = strings.ToLower(command)
+		// Match a function
+		switchboard(command, args)
+	}
+}
+
+func getInput() string {
+	// Create a keyboard reader
+	keyboard := bufio.NewReader(os.Stdin)
+	// Read a line of input
+	line, e := keyboard.ReadString('\n')
+	// Print out any errors
+	if e != nil {
+		fmt.Fprintln(os.Stderr, e)
+	}
+
+	// Trim \r\n for Windows
+	if runtime.GOOS == "windows" {
+		line = strings.TrimRight(line, "\r\n")
+	} else {
+		line = strings.TrimRight(line, "\n")
+	}
+
+	return line
+}
+
+func parseCommand(line string) (string, []string) {
+	// Separate the arguments
+	input := strings.Split(line, " ")
+	command := input[0]
+	args := input[1:]
+	// Return command and arguments
+	return command, args
+}
+
+func switchboard(command string, args []string) {
+	// Route the command to call the proper function
+	switch command {
+	case "mkdir":
+		Mkdir(args)
+	case "exit":
+		os.Exit(0)
+	}
+}
