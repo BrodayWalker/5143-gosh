cf527b5db786a972a1f93d5c84fc43f163f758e0
Broday Walker <brodaywalker@gmail.com>
2020-02-11 15:09:28 -0600

    pwd update

--- gosh/pwd.go
+++ gosh/pwd.go
@@ -10,8 +10,11 @@ import (
 // Pwd prints the name of the working directory.
 func Pwd(args []string) {
 	if len(args) == 0 {
+		// Replace these
+		var a *uint16
 		// Print the working directory
-		dir, err := sys.GetFullPathName(nil, nil, nil)
-		fmt.Printf(dir)
+		dir, err := sys.GetFullPathName(a, 4, nil, nil)
+		fmt.Println(dir)
+		fmt.Println(err)
 	}
 }
