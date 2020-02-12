ca570167e36be26c256c34e1843fcfa00cb3a1a4
Broday Walker <brodaywalker@gmail.com>
2020-02-11 20:11:43 -0600

    fixed pwd

--- gosh/pwd.go
+++ gosh/pwd.go
@@ -11,19 +11,13 @@ import (
 func Pwd(args []string) {
 	if len(args) == 0 {
 		// Replace these
-		var a *uint16
+		//var a *uint16
 		// Print the working directory
-<<<<<<< HEAD
 		var one, three *uint16
 		var two uint32
 		var four **uint16
 
 		dir, _ := sys.GetFullPathName(one, two, three, four)
 		fmt.Printf("%d", dir)
-=======
-		dir, err := sys.GetFullPathName(a, 4, nil, nil)
-		fmt.Println(dir)
-		fmt.Println(err)
->>>>>>> cf11578b0429f4ce410d719b9148bc1c4fffec14
 	}
 }
