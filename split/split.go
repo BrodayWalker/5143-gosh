e88028960a9d09a26aa3957704eca072d1bb6e31
Pirhomega <35641464+Pirhomega@users.noreply.github.com>
2020-02-15 14:44:50 -0600

    got split's basic functions working

--- split/split.go
+++ split/split.go
@@ -2,26 +2,53 @@ package main
 
 import (
 	"fmt"
-	// "log"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"strconv"
 	// "strings"
 )
 
 //Split : split [option] [input [prefix]]
 //example: split -n3 ooga.txt
 func main() {
-	input := [1]string{"asdf.txt"}
+	input := [1]string{"C:\\Users\\Owner\\Desktop\\asdfasdf\\asdf.txt"}
 
 	//outer loop will iterate through all elements in args
 	//inner loop will iterate through each character in element
 	for word := range input {
-		fmt.Println(input[word] + " ")
 		for char := range input[word] {
-			fmt.Println(string(input[word][char]) + " ")
-
 			//switch cases will only cover single-dashed flags for now
 			switch string(input[word][char]) {
 			case "-":
-				flags(input[word][1:])
+				flags(input[word][1:]) //process flags
+			default:
+				fmt.Println("Ran default!")
+				counter := 42
+				file, err := os.Open(input[word]) //open file from input string array
+				if err != nil {
+					log.Fatal(err)
+				}
+				//this variable will hold the sections of data we will be writing to separate files
+				data := make([]byte, 100)
+				for {
+					//create a "sub-file" with the same name as the parent but with a counter value appended to the front
+					file2, err := os.Create(strconv.Itoa(counter) + filepath.Base(input[word]))
+					if err != nil {
+						log.Fatal(err)
+					}
+					//read the data from 'file' into array 'data'
+					_, err = file.Read(data)
+					//write contents of 'data' to 'file2'
+					file2.Write(data)
+					//if we've reached the end of the file, quit looping through it
+					if err == io.EOF {
+						break
+					}
+					counter++
+				}
+				break
 			}
 		}
 	}
