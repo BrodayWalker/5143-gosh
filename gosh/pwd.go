75cf4154cef37c246a1c51dbcc83df7cdb4f230f
Broday Walker <brodaywalker@gmail.com>
2020-02-15 13:21:41 -0600

    completed pwd

--- gosh/pwd.go
+++ gosh/pwd.go
@@ -2,22 +2,15 @@ package main
 
 import (
 	"fmt"
-	// If not found, use command "go get golang.org/x/sys/windows"
-	// sys is and alias
-	sys "golang.org/x/sys/windows"
+	"os"
 )
 
 // Pwd prints the name of the working directory.
 func Pwd(args []string) {
 	if len(args) == 0 {
-		// Replace these
-		//var a *uint16
-		// Print the working directory
-		var one, three *uint16
-		var two uint32
-		var four **uint16
-
-		dir, _ := sys.GetFullPathName(one, two, three, four)
-		fmt.Printf("%d", dir)
+		dir, err := os.Getwd()
+		if err == nil {
+			fmt.Println(dir)
+		}
 	}
 }
