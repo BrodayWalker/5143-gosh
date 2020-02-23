f0370ad764068be839ae8949b94bb4bd651614d5
Broday Walker <brodaywalker@gmail.com>
2020-02-22 18:51:52 -0600

    ls with -l works with all valid paths

--- gosh/ls.go
+++ gosh/ls.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"regexp"
+	"os"
+	"path/filepath"
+	"time"
 )
 
 func init() {
@@ -14,6 +16,8 @@ func init() {
 // Ls lists all files and directories in a specified folder. Currently, no
 // flags are supported.
 func Ls(args []string) {
+	argList, flags := ArgSplitter(args)
+	var path string = ""
 
 	// ReadDir reads the directory named by dirname and returns a list of
 	// directory entries sorted by filename. The entries are a FileInfo
@@ -30,48 +34,68 @@ func Ls(args []string) {
 	*/
 
 	// If no arguments, list all file and folder names only
-	if len(args) == 0 {
-		files, err := ioutil.ReadDir(".")
-		if err != nil {
-			fmt.Println(err)
-		}
-		for _, file := range files {
-			fmt.Printf(file.Name() + " ")
-		}
-		fmt.Printf("\n")
-	} else {
-		// Handle flags
-		// Compile the expression to test against
-		// A flag must be in args[0] or it will not be caught
-		var flags, _ = regexp.Compile(`^-{1,2}(\d|\D)`)
-		if flags.MatchString(args[0]) {
-			// Print flags for testing
-			fmt.Printf("Flags: ")
-			for i := 0; i < len(args); i++ {
-				fmt.Printf(args[i])
+	if len(argList) == 0 && len(flags) == 0 {
+		defaultPrint(".")
+	} else if len(argList) > 0 && len(flags) == 0 {
+		// Different path than current working directory, no flags passed
+		path = buildPath(argList[0])
+		defaultPrint(path)
+	} else if len(argList) == 0 && len(flags) > 0 {
+		longPrint(".")
+	} else if len(argList) > 0 && len(flags) > 0 {
+		path = buildPath(argList[0])
+		for _, v := range flags {
+			if v == "l" {
+				longPrint(path)
 			}
-			fmt.Printf("\n")
-			// -l
-			var l, _ = regexp.Match("l", []byte(args[0]))
-			if l {
-				files, _ := ioutil.ReadDir(".")
-				for _, file := range files {
-					// Print permissions
-					fmt.Printf("%s ", file.Mode())
-					// Print owner fields
-					// This is not implemented as Windows returns -1 for
-					// the group and owner fields
-					// Print size
-					fmt.Printf("%d ", file.Size())
-					// Print date
-					fmt.Printf("%v ", file.ModTime())
-					// Print file/folder name
-					fmt.Printf(file.Name() + " ")
-					fmt.Printf("\n")
-				}
-			}
-		} else {
+		}
+	}
+}
 
-		}
+func buildPath(path string) string {
+	absPath, fpErr := filepath.Abs(path)
+	// Print error
+	if fpErr != nil {
+		fmt.Println("Absolute path error")
+	}
+	// Get FileInfo struct for our path
+	pathInfo, statErr := os.Stat(absPath)
+	// If path is not valid
+	// Path must exist and must be a directory
+	if statErr != nil || pathInfo.IsDir() == false {
+		fmt.Println(statErr)
+		return ""
+	}
+
+	return absPath
+}
+
+func defaultPrint(path string) {
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, file := range files {
+		fmt.Printf(file.Name() + " ")
+	}
+	fmt.Printf("\n")
+}
+
+func longPrint(path string) {
+	files, _ := ioutil.ReadDir(path)
+	for _, file := range files {
+		// Print permissions
+		fmt.Printf("%s ", file.Mode())
+		// Print owner fields
+		// This is not implemented as Windows returns -1 for
+		// the group and owner fields
+		// Print size
+		fmt.Printf("%12d ", file.Size())
+		// Print date
+		t := file.ModTime()
+		fmt.Printf("%v ", t.Format(time.UnixDate))
+		// Print file/folder name
+		fmt.Printf(file.Name() + " ")
+		fmt.Printf("\n")
 	}
 }
