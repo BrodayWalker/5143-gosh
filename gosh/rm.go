0c2aae06b2c037e6b03befbad8dece29403acd3a
Student <student@bo103-lab-0211.labs.msu>
2020-02-26 13:54:39 -0600

    recursive rm on unix works

--- gosh/rm.go
+++ gosh/rm.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 func init(){
@@ -13,10 +14,48 @@ func init(){
 // Rm removes a folder or file
 func Rm(args []string) {
 	if len(args) != 0 {
-		rmMe := args[0]
-		err := os.Remove(rmMe)
-		if err != nil {
-			fmt.Printf("%v\n", err)
+		argList, flags := ArgSplitter(args)
+		// Debug
+		fmt.Println(argList)
+		fmt.Println(flags)
+
+		// Get absolute path
+		path, absErr := filepath.Abs(argList[0])
+		if absErr != nil {
+			fmt.Println(absErr)
+		}
+
+		// Use first non-flag argument as file or folder to delete
+		if len(argList) > 0 {
+			// Plain delete; must be a file
+			if len(flags) == 0 {
+				// Try to remove the file
+				err := os.Remove(path)
+				if err != nil {
+					fmt.Println(err)
+				}
+			} else if len(flags) > 0 {
+				// Check the flags
+				rFlag := false
+				fFlag := false
+				// Loop through flag array
+				for _, v := range flags {
+					if v == "r" {
+						rFlag = true
+					}
+					if v == "f" {
+						fFlag = true
+					}
+				}
+
+				// -f is not currently supported so rm -rf <some_folder> works
+				// the same as rm -r <some_folder>
+				if (rFlag == true && fFlag == true) || (rFlag == true && fFlag == false) {
+					os.RemoveAll(path)
+				}
+			
+			}
 		}
 	}
+		
 }
