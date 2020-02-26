bee57217299d010765bb67f6c9d90c2f788ad43d
Broday Walker <brodaywalker@gmail.com>
2020-02-26 07:52:06 -0600

    home directory functionality

--- gosh/cd.go
+++ gosh/cd.go
@@ -14,8 +14,12 @@ func init() {
 func Cd(args []string) {
 	// There should only be one argument
 	if len(args) == 1 {
+		dir := args[0]
+		if dir == "~" {
+			dir, _ = os.UserHomeDir()
+		}
 		// Try to change directory
-		err := os.Chdir(args[0])
+		err := os.Chdir(dir)
 		if err != nil {
 			fmt.Printf("%v\n", err)
 		}
