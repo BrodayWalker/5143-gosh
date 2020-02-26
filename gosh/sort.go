080b6eb22f94897a7dddd304f9a5a3a0505ea401
Pirhomega <35641464+Pirhomega@users.noreply.github.com>
2020-02-26 11:27:41 -0600

    intiated sort

---
+++ gosh/sort.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+)
+
+func init() {
+	// Add this command's function to the command mapping
+	ComMap["sort"] = Sort
+}
+
+// Sort ..
+func Sort(args []string) {
+
+}
