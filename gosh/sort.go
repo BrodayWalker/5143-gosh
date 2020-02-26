3023255e79332d36187f87c60f22136c338e51e9
Pirhomega <35641464+Pirhomega@users.noreply.github.com>
2020-02-26 17:00:37 -0600

    added documentation to sort

--- gosh/sort.go
+++ gosh/sort.go
@@ -12,30 +12,15 @@ var numDict = map[string]int{"0": 0, "1": 0, "2": 0, "3": 0, "4": 0, "5": 0, "6"
 var lowerDict = map[string]int{"a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0, "h": 0, "i": 0, "j": 0, "k": 0, "l": 0, "m": 0, "n": 0, "o": 0, "p": 0, "q": 0, "r": 0, "s": 0, "t": 0, "u": 0, "v": 0, "w": 0, "x": 0, "y": 0, "z": 0}
 var upperDict = map[string]int{"A": 0, "B": 0, "C": 0, "D": 0, "E": 0, "F": 0, "G": 0, "H": 0, "I": 0, "J": 0, "K": 0, "L": 0, "M": 0, "N": 0, "O": 0, "P": 0, "Q": 0, "R": 0, "S": 0, "T": 0, "U": 0, "V": 0, "W": 0, "X": 0, "Y": 0, "Z": 0}
 
-// // a cell will hold a line's first character and its line number
-// type cell struct {
-// 	char    string
-// 	lineNum int
-// }
-
-// // will be sorted in place of sorting the file itself
-// var fileArray []cell
-
 func init() {
 	// Add this command's function to the command mapping
 	ComMap["sort"] = Sort
 }
 
-// Sort ..
-// 1. I want to implement a dictionary {key:value} where the key is a character found in the input file to be sorted, and value
-// 		is the number of occurances of that key in the file. Keys are only the first characters of each line.
-// 2. sortFile will first run through the entire input file and update the dictionary.
-// 3. Create an array of structs, where the struct is a string and an int: the first character of a line and the line number, respectively
-// 4. Sort the array of structs by the character
-// 5. Now, for each series of elements where the characters are the same, replace the struct's characters with the next character in the line
-// 6. Sort the sub array, repeating 5 and 6 until that entire subarray is sorted.
-// 7. Move on to the next sub array that have the same characters, repeating 5 through 7 until the entire array is sorted.
-// 8. Print, using the line numbers of the struct, the sorted lines to Stdout.
+// Sort performs a brute-force algorithm to sort each line of an input file
+// by the first character. It currently does not sort past the first character
+// Usage:
+//				sort input1 input2 inputN
 func Sort(args []string) {
 	argsSize := len(args)
 	if argsSize == 1 {
@@ -50,6 +35,11 @@ func Sort(args []string) {
 	}
 }
 
+// Since sort on bash doesn't use the ascii order, regular sorting doesn't work.
+// This function will search the input files four times, independently searching
+// for either numbers, lowercase letters, uppercase letters, or symbols.
+// It then sorts each case and dumps them into Stdout in the order symbols, numbers,
+// lowercase letters, uppercase letters.
 func sortFile(file string) {
 	var (
 		numbers, lowers, uppers, symbols = []string{}, []string{}, []string{}, []string{}
@@ -58,13 +48,18 @@ func sortFile(file string) {
 	inputFile, _ := os.OpenFile(file, os.O_RDONLY, 0755)
 	scanner := bufio.NewScanner(inputFile)
 	for scanner.Scan() {
+		// we check if Scan() read in a blank line. If so, skip it
 		if scanner.Text() != "" {
+			// If the first character of the line is a number, add it to the number array
 			if _, numIn := numDict[string(scanner.Text()[0])]; numIn {
 				numbers = append(numbers, scanner.Text())
+				// If the first character of the line is a lowercase letter, add it to the number array
 			} else if _, lowerIn := lowerDict[string(scanner.Text()[0])]; lowerIn {
 				lowers = append(lowers, scanner.Text())
+				// If the first character of the line is a uppercase letter, add it to the number array
 			} else if _, upperIn := upperDict[string(scanner.Text()[0])]; upperIn {
 				uppers = append(uppers, scanner.Text())
+				// everything else, add it to the number array
 			} else {
 				symbols = append(symbols, scanner.Text())
 			}
@@ -72,10 +67,13 @@ func sortFile(file string) {
 	}
 	os.Remove("sort.tmp")
 	inputFile.Close()
+	// sort each of the four arrays
 	sort.Strings(symbols)
 	sort.Strings(numbers)
 	sort.Strings(lowers)
 	sort.Strings(uppers)
+	// print them all to Stdout in the bash way of sorting:
+	// symbols, numbers, lowers, then uppers
 	for element = range symbols {
 		fmt.Println(symbols[element])
 	}
@@ -89,7 +87,3 @@ func sortFile(file string) {
 		fmt.Println(uppers[element])
 	}
 }
-
-// for ele := range args {
-// 	fmt.Println(args[ele])
-// }
